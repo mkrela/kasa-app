f897b1940d4317366334dd2606b4282eb5f83108
m.krela <m.krela@icloud.com>
2023-05-19 14:54:21 +0200

    Ajout de commentaires

--- src/hooks/useApartment.jsx
+++ src/hooks/useApartment.jsx
@@ -6,17 +6,27 @@ export function useApartment() {
   const location = useLocation();
 
   useEffect(() => {
+    // Création d'un objet AbortController pour annuler la requête fetch si nécessaire
     const abortController = new AbortController();
+
+    // Effectue une requête fetch pour récupérer les données du fichier "db.json"
     fetch("db.json", { signal: abortController.signal })
       .then((res) => res.json())
       .then((flats) => {
+        // Recherche de l'appartement correspondant à l'ID passé dans location.state.apartmentId
         const flat = flats.find((flat) => flat.id === location.state.apartmentId);
+        // Met à jour l'état flat avec l'appartement trouvé
         setFlat(flat);
       })
       .catch(console.error);
+
+    // Retourne une fonction de nettoyage qui sera exécutée lors du démontage du composant
     return () => {
+      // Annule la requête fetch en appelant la méthode abort() de l'AbortController
       abortController.abort();
     };
   }, []);
+
+  // Retourne l'état flat qui contient l'appartement
   return flat;
 }
--- src/hooks/useApartments.jsx
+++ src/hooks/useApartments.jsx
@@ -2,12 +2,15 @@ import { useState, useEffect } from "react";
 
 export const useApartments = () => {
   const [apartments, setApartments] = useState([]);
+
   useEffect(() => {
+    // Effectue une requête fetch pour récupérer les données du fichier "db.json"
     fetch("db.json")
       .then((res) => res.json())
       .then((res) => setApartments(res))
       .catch(console.error);
   }, []);
 
+  // Retourne l'état apartments qui contient les données des appartements
   return apartments;
 };
